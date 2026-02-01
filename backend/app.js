a22bc6e50d78fead74af5d6684ed67493e8645c1
Shadan0786 <Shadanashrafi82@gmail.com>
2026-02-02 00:14:22 +0530

    E-commerce App

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on a port ${PORT}`)
+})
